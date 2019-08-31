672f5881aed297ba44a75f618bb35d39a04ec6f4
Valentin Manea <valentin.manea@gmail.com>
2019-08-31 11:57:23 +0300

    Enable 64bit core

--- lineage_surfacepro3.mk
+++ lineage_surfacepro3.mk
@@ -1,3 +1,4 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/microsoft/surfacepro3/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
